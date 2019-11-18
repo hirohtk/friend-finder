81ee1cd137e76eebf74167ae6a476bbe731be5bf
Kensen <hirohtk@gmail.com>
2019-11-17 20:51:09 -0800

    working on matching function- good exposure to these array methods

--- server.js
+++ server.js
@@ -11,42 +11,85 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/app/public/home.html"));
-  });
-  
-  app.get("/survey", function(req, res) {
+});
+
+app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "/app/public/survey.html"));
-  });
+});
 
-  // Displays all people
-  app.get("/api/friends", function (req, res) {
-      return res.json(friends);
-  });
-  
-  var friends = [
+// Displays all people
+app.get("/api/friends", function (req, res) {
+    return res.json(friends);
+});
+
+var friends = [
     {
-      name: "deep man",
-      photo: "images/sea-pig-hand-mouth-random-man-37958727-480-360.jpg",
-      scores: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ,10]
+        name: "George",
+        photo: "http://images6.fanpop.com/image/photos/37900000/-standing-up-random-man-37958716-200-300.jpg",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     },
-  ];
+    {
+        name: "Kil'Jaeden, the Deciever",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiAjOax8_LlAhVDrZ4KHaw1A1sQjRx6BAgBEAQ&url=https%3A%2F%2Fwowwiki.fandom.com%2Fwiki%2FKil%2527jaeden&psig=AOvVaw1zSwWXVO-x91A1El4AgnqX&ust=1574136985697436",
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+        name: "Tyson Chandler",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjpharv8vLlAhXJqZ4KHUgwBGUQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.espn.com%2Fnba%2Fplayer%2F_%2Fid%2F984%2Ftyson-chandler&psig=AOvVaw2wh96knYpEqw5xQw1GYQ3v&ust=1574136845055366",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    }
+];
 
-  // Create New 'People' - takes in JSON input
-app.post("/api/friends", function(req, res) {
+// Create New 'People' - takes in JSON input
+app.post("/api/friends", function (req, res) {
 
     var newPerson = req.body;
 
     console.log(newPerson);
-  
+
     friends.push(newPerson);
-  
+
+    findMatch(newPerson);
+
     res.json(newPerson);
 });
-  
-  // Starts the server to begin listening
-  // =============================================================
-  app.listen(PORT, function() {
+
+function findMatch(input) {
+    let newPersonArray = input.scores;
+
+    let comparedArrays = [];
+
+    let comparedArraySums = [];
+
+    // FOR EACH FRIEND, MINUS ONE SINCE NEWEST ALREADY GETS APPENDED TO THE END AT THIS POINT...
+    for (let i = 0; i < friends.length - 1; i++) {
+    // ..RUN THIS FOR LOOP WHICH MAKES A NEW ARRAY THAT TAKES THE DIFFERENCE OF EACH VALUE AT SCORE INDEX 
+    // AND STORES IT AS A NEW INDEX IN THIS NEW ARRAY.  IT WORKS ON SAME FRIEND[i] IN ONE OF THIS FOR LOOP UNTIL DONE, THEN MOVE TO NEXT.
+        for (let j = 0; j < 9; j++) {
+            comparedArrays[i] = newPersonArray.map(x => Math.abs(x - friends[i].scores[j]));
+        }
+    }
+    console.log(comparedArrays);
+    for (let k = 0; k < comparedArrays.length; k++) {
+        let sum= 0;
+        for (let l = 0; l < 9; l++) {
+            sum += comparedArrays[k][l];
+        }
+        comparedArraySums.push(sum);
+        // WHAT THIS SHOULD RETURN IS ONE VALUE IN ARRAYSUMS ARRAY FOR EACH PERSON 
+    }
+    console.log(comparedArraySums);
+
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
+    const min = Math.min.apply(null, comparedArraySums);
+
+    comparedArraySums.indexOf(min);
+}
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
-  
+});
